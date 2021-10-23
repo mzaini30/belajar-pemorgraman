a05954c4f83633ec87abbec8095ca0d3ed39d69e
mzaini30 <muhzaini30@gmail.com>
2021-10-24 01:00:05 +0800

    oke

--- src/pages/post/_komentar.svelte
+++ src/pages/post/_komentar.svelte
@@ -1,6 +1,16 @@
 <script>
+	import redaxios from 'redaxios'
+	const {post} = redaxios
+	import {stringify} from 'qs'
+	 
 	const browser = !import.meta.env.SSR
 	const api = 'adef2df9c083a6f7327bcc1a7247e8d0' // id, nama, link, komentar, slug
+	const sql = 'https://excalibur.mabaiz.web.id/sql.php'
+
+	let semuaKomentar
+	let nama
+	let link 
+	let komentar
 
 	// JSON.stringify(location.pathname)
 
@@ -11,28 +21,73 @@
 	kirim
 	insert into [db] (nama, link, komentar, slug) values ([nama], [link], [komentar], [slug])
 	*/
+
+	if (browser) {
+		if (localStorage.nama && localStorage.link) {
+			nama = localStorage.nama
+			link = localStorage.link 
+		}
+	}
+
+	async function ambilData(){
+		if (browser) {
+			const {data} = await post(sql, stringify({
+				id: api,
+				kunci: 'ambil',
+				slug: location.pathname
+			}))
+			semuaKomentar = data
+		}
+	}
+	ambilData()
+
+	async function kirim(){
+		if (browser) {
+			const proses = await post(sql, stringify({
+				id: api,
+				kunci: 'kirim',
+				nama,
+				link,
+				komentar,
+				slug: location.pathname
+			}))
+			if (proses) {
+				localStorage.nama = nama
+				localStorage.link = link
+				alert('Komentar berhasil ditambahkan.')
+				komentar = ''
+				ambilData()
+			}
+		}
+	}
 </script>
 
 <div class="komentar">
 	<p class="judul">Yuk Berkomentar...</p>
-	<form action="">
+
+	<form on:submit|preventDefault={kirim}>
 		<label>Nama</label>
-		<input class="input" required type="text">
+		<input bind:value={nama} class="input" required type="text">
+
 		<label>Link Blog</label>
-		<input type="text" class="input" required>
+		<input type="url" bind:value={link} class="input" required>
+		
 		<label class="block">Komentar</label>
-		<textarea required name="" id="" cols="30" class="input" rows="10"></textarea>
+		<textarea required name="" id="" bind:value={komentar} cols="30" class="input" rows="10"></textarea>
+		
 		<input value="Kirim Komentar" class="submit" type="submit" >
 	</form>
 
-	<p class="judul">Semua Komentar...</p>
+	{#if semuaKomentar}
+		<p class="judul">Semua Komentar...</p>
 
-	<ol>
-		{#each Array(2) as x}
-			<li>
-				<a rel="nofollow" href="/">Kucing Garong</a>
-				<p>halo</p>
-			</li>
-		{/each}
-	</ol>
+		<ol>
+			{#each semuaKomentar as x}
+				<li>
+					<a rel="nofollow" href="/">Kucing Garong</a>
+					<p>halo</p>
+				</li>
+			{/each}
+		</ol>
+	{/if}
 </div>
