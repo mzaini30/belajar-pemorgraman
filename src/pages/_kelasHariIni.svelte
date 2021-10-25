57670750d369c459fcc089dbe8b323d79950f3bf
mzaini30 <muhzaini30@gmail.com>
2021-10-25 17:43:30 +0800

    oke

---
+++ src/pages/_kelasHariIni.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let semuaKategori
+
+	const tanggal = new Date().setHours(0,0,0,0) / (10 ** 5)
+	const bagi = tanggal % semuaKategori.length
+	const kelasHariIni = semuaKategori[bagi]
+</script>
+
+<p class="info"><strong>Kelas hari ini:</strong> <a href='/kelas/{kelasHariIni}'>Kelas {kelasHariIni}</a></p>
