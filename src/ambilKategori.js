fdda887e2aa023cbf043bb1086b702f8f9674fb5
mzaini30 <muhzaini30@gmail.com>
2021-11-28 12:30:00 +0800

    Ubah codebase

--- src/ambilKategori.js
+++
@@ -1,8 +0,0 @@
-export function ambilKategori(semua){
-	let semuaKategori = []
-	for (let x of semua){
-		semuaKategori = [...semuaKategori, x.kategori]
-	}
-	semuaKategori = [...new Set(semuaKategori)]
-	return semuaKategori.sort()
-};
--- src/components/IklanMelayang.svelte
+++
@@ -1,5 +0,0 @@
-<!--
-<div>
-	<script type='text/javascript' src='//coatoctopus.com/f8/e9/d6/f8e9d677d865ea5aa2328030fc2e760a.js'></script>
-</div>
--->
