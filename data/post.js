a0d514ac6e573b6d8bcebc4f7edcd66169c088e5
mzaini30 <mzaini30@users.noreply.github.com>
2021-12-01 16:12:46 +0000

    deploy: 2a96d6944fb5f72dca47c69065ed799d4f068593

--- data/post.js
+++ data/post.js
@@ -34,4 +34,5 @@ export default [
 	{judul: 'Mengenal Mini SSG Zen', slug: 'mengenal-mini-ssg-zen', kelas: 'Mini SSG Zen'},
 	{judul: 'Berbagai Modul JavaScript yang Bermanfaat', slug: 'berbagai-modul-javascript-yang-bermanfaat', kelas: 'JavaScript'},
 	{judul: 'Fullstack Tanpa Backend dengan Excalibur', slug: 'fullstack-tanpa-backend-dengan-excalibur', kelas: 'JavaScript'},
+	{judul: 'Optimasi Blog', slug: 'optimasi-blog', kelas: 'Blogspot'},
 ]
