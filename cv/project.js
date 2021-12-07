233e744e8835c2ab737e5144ac86620a3fbae0eb
mzaini30 <mzaini30@users.noreply.github.com>
2021-12-07 22:43:16 +0000

    deploy: 81a17b684946659f22051bd6b38d4ced785e1ddf

---
+++ cv/project.js
@@ -0,0 +1,14 @@
+// balik, ambil 5
+
+export default [
+	{
+		tanggal: 'Nov 2021',
+		nama: 'Mini SSG Zen',
+		bentuk: 'Package JavaScript',
+		detail: [
+			'Merupakan pengembangan dari mini-ssg dengan tambahan banyak fitur',
+			'Static Site Generator yang cepat mengolahnya',
+			'https://npm.im/mini-ssg-zen'm
+		]
+	}
+]
---
+++ cv/skill.js
@@ -0,0 +1,1 @@
+export default[{ringkasan:"Bahasa Pemrograman",detail:["JavaScript","Lua","Python","PHP","Lisp"]},{ringkasan:"Front End",detail:["Vue","jQuery","Petite Vue","Fetch API"]},{ringkasan:"Back End",detail:["Excalibur"]}];
