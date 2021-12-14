1c90f7b1d191fd5b68dcbecf859d66dc02ee036d
mzaini30 <muhzaini30@gmail.com>
2021-12-15 07:53:46 +0800

    Kasih jawaban komentar

--- static/data/api.js
+++ static/data/api.js
@@ -1,2 +1,5 @@
 export const komentar = 'adef2df9c083a6f7327bcc1a7247e8d0'; // id, nama, link, komentar, slug
+export const jawaban = '2e51cfff58b38a18275f85c226c795fc' // id, komentar, idTarget, slug
+export const auth = '954d8cf549af36d42580110e5d2660c1' // id, username, password
+
 export const sql = 'https://excalibur.mabaiz.web.id/sql.php';
